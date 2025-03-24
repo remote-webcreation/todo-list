0d9e50857a6bcdbb9fb171db5c2746baec2f3d07
Mandy Jendroska <soulfreelancer@yahoo.com>
2025-03-24 21:15:55 +0100

    finished input and removal

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ let close = document.getElementsByClassName("close");
 
 
 for (i=0; i<close.length; i++) {
-    let i;
+    
     close[i].onlick = function(){
         let div = this.parentElement;
         div.style.display = "none";
@@ -25,6 +25,33 @@ btn.addEventListener("click", function(ev) {
         ev.target.classList.toggle("checked");
     }
 }, false);
+
+function newItem(){
+    let li = document.createElement("li");
+    let inputValue = document.getElementById("input").value;
+    let t = document.createTextNode(inputValue);
+    li.appendChild(t);
+
+    if(inputValue === ""){
+        alert("please write something!");
+    } else {
+        document.getElementById("item").appendChild(li);
+    }
+    document.getElementById("input").value = "";
+    let span = document.createElement("SPAN");
+    let txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    li.appendChild(span);
+
+    let close = document.getElementsByClassName("close");
+    for (let i = 0; i < close.length; i++) {
+        close[i].onclick = function () {
+            let div = this.parentElement;
+            div.style.display = "none";
+        };
+    }
+};
 
 
 
